3fdc7a2ad469a8f202bd5596115273ff6459b7c9
guanz808 <jiwanaga@hotmail.com>
2023-03-04 12:04:42 -1000

    Update ubuntu_setup01.sh

--- ubuntu_setup01.sh
+++ ubuntu_setup01.sh
@@ -59,7 +59,7 @@ mkdir -p ~/.config/nvim/colors
 curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/nvim/init.vim
 
 # install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install ZSH
