f33cf8a71bcc00932784cf152cc7e183861341e4
Jayson <jiwanaga@hotmail.com>
2023-03-04 12:16:27 -1000

    configured for lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+
+-- IMPORTS
+require("plugins")
+require("options")
+require("keymaps")
+require("variables")
---
+++ .config/nvim/lua/variables.lua
@@ -0,0 +1,5 @@
+--[[ variables.lua ]]
+
+local g = vim.g
+g.t_co = 256
+g.background = "dark"
