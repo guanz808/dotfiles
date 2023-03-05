82f998513380bb9cd495bf3ec92a404db74ceb89
Jayson <jiwanaga@hotmail.com>
2023-03-04 15:47:07 -1000

    repo download

--- README.md
+++ README.md
@@ -3,9 +3,20 @@
 ### To run the install and configuration on an Ubuntu container:
 ```
 apt update && apt upgrade -y
-apt install curl -y
+apt install curl git -y
 curl -o ~/ubuntu_setup01.sh https://raw.githubusercontent.com/guanz808/dotfiles/main/ubuntu_setup01.sh
 cd ~
 chmod +x ubuntu_setup01.sh
 ./ubuntu_setup01.sh -y
+```
+
+alternative:
+```
+apt update && apt upgrade -y
+apt install curl git -y
+git clone https://github.com/guanz808/dotfiles.git
+chmod +x ~/dotfiles/ubuntu_setup01.sh
+~/dotfiles/ubuntu_setup01.sh -y
+
+
 ```
--- ubuntu_setup01.sh
+++ ubuntu_setup01.sh
@@ -32,12 +32,27 @@ wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003
 unzip Hack-v3.003-ttf.zip -d /usr/local/share/fonts -yellow -y 
 rm -rf Hack-v3.003-ttf.zip
 
-# Install neofetch
+# move files from the dotfile to the root
+# Download dotfiles from GitHub
+git clone https://github.com/guanz808/dotfiles.git
+
+### Remove the .config directory ###
+rm -rf .config
+
+# Move necessary files to home directory and remove unnecessary files
+mv -f dotfiles/{.bashrc,.zshrc} ~
+mv -f dotfiles/.config ~
+rm -rf dotfiles/{.git,LICENSE,README.md}
+
+# Cleanup
+rm -rf dotfiles
+
+### Install neofetch ###
 apt install -y neofetch
 
 # Download neofetch config.conf file
-mkdir -p ~/.config/neofetch
-curl -o ~/.config/neofetch/config.conf https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/neofetch/config.conf
+# mkdir -p ~/.config/neofetch
+# curl -o ~/.config/neofetch/config.conf https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/neofetch/config.conf
 
 # install neovim
 # apt install neovim -y
@@ -47,47 +62,49 @@ curl -LO https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.
 
 # Create ~/.config/nvim folder
 mkdir -p ~/.config/nvim
+curl -o ~/.config/nvim/ https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/nvim/
 
 # install neovim and cleanup
 apt install ./nvim-linux64.deb
 rm -rf ./nvim-linux64.deb
 
 # Download neovim configuration files
-cd ~
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/nvim/colors
-curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/nvim/init.vim
+#cd ~
+#mkdir -p ~/.config/nvim
+#mkdir -p ~/.config/nvim/colors
+#curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/nvim/init.vim
 
 # install vim-plug
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Install ZSH
-apt install zsh -y
-chsh -s $(which zsh)  # make zsh the default shell
-# Install OMZ
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-# Install OMZ syntax highlighting and autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Install necessary packages for starship
-apt-get update &&  apt-get install curl -y
-
-# Install Starship
-wget https://starship.rs/install.sh -O starshipInstall.sh
-chmod +x starshipInstall.sh
-./starshipInstall.sh -y
-rm -rf starshipInstall.sh
-
-# Add starship.toml configuration file
-cd ~
-mkdir -p ~/.config/starship
-curl -o ~/.config/starship/starship.toml https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/starship/starship.toml
-
-# Download ZSH .zshrc config file
-curl -o .zshrc https://raw.githubusercontent.com/guanz808/dotfiles/main/.zshrc
-
-# Start ZSH
-zsh
+##  # Install ZSH
+##  apt install zsh -y
+##  chsh -s $(which zsh)  # make zsh the default shell
+##  # Install OMZ
+##  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+##  
+##  # Install OMZ syntax highlighting and autosuggestions
+##  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+##  git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+##  
+##  # Install necessary packages for starship
+##  apt-get update &&  apt-get install curl -y
+##  
+##  # Install Starship
+##  wget https://starship.rs/install.sh -O starshipInstall.sh
+##  chmod +x starshipInstall.sh
+##  ./starshipInstall.sh -y
+##  rm -rf starshipInstall.sh
+##  
+##  # Add starship.toml configuration file
+##  cd ~
+##  mkdir -p ~/.config/starship
+##  curl -o ~/.config/starship/starship.toml https://raw.githubusercontent.com/guanz808/dotfiles/main/.config/starship/starship.toml
+##  
+##  # Download ZSH .zshrc config file
+##  curl -o .zshrc https://raw.githubusercontent.com/guanz808/dotfiles/main/.zshrc
+##  
+##  # Start ZSH
+##  zsh
+##  
