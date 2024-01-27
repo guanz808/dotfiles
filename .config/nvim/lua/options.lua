33091b1740c47e7b632935f51c8bfe7a7a0b205b
Jayson <jiwanaga@hotmail.com>
2024-01-26 18:40:38 -1000

    comment opt.colorcolumn

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,7 +2,7 @@
 local opt = vim.opt
 
 -- [[ Context ]]
-opt.colorcolumn = '80'           -- str:  Show col for max line length
+-- opt.colorcolumn = '80'           -- str:  Show col for max line length
 opt.number = true                -- bool: Show line numbers
 opt.relativenumber = true        -- bool: Show relative line numbers
 opt.scrolloff = 4                -- int:  Min num lines of context
