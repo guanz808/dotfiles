3ab914ba151c9b2843d1ed20d276e437a8317254
guanz808 <jiwanaga@hotmail.com>
2024-05-06 17:32:27 -1000

    cleanup

--- ubuntu_setup01.sh
+++ ubuntu_setup01.sh
@@ -10,11 +10,6 @@ git clone https://github.com/guanz808/dotfiles.git
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 # Install Terraform
-#apt-get update && apt-get install -y gnupg software-properties-common curl
-#curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-#apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-#apt-get update && apt-get install terraform -y
-
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
